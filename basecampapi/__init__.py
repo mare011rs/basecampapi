48c509dbfe47da400928f4a3572445bac0a658a2
mare011rs <mare011rs@gmail.com>
2022-12-27 20:44:00 +0100

    added authorization import

--- basecampapi/__init__.py
+++ basecampapi/__init__.py
@@ -4,6 +4,6 @@ import os
 import json
 from mimetypes import MimeTypes
 
-from .basecamp import Basecamp
+from .basecamp import Basecamp, Authorization
 from .endpoints.camprife import Campfire
 from .endpoints.messageboard import MessageBoard
