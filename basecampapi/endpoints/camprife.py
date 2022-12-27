423a46766cc85bed62a3ea064eb75bfd8dae1fc8
mare011rs <mare011rs@gmail.com>
2022-12-27 20:59:27 +0100

    removed object variables from Campfire

--- basecampapi/endpoints/camprife.py
+++ basecampapi/endpoints/camprife.py
@@ -1,25 +1,24 @@
 class Campfire:
         
     def __init__(self, project_id, campfire_id, session):
-        self.project_id = project_id
-        self.campfire_id = campfire_id
-        self.files = session.files
-        self.access_token = session.access_token
-        self.base_url = session.base_url
-        self.headers = {
-            'Authorization': 'Bearer '+ self.access_token,
+        self.__project_id = project_id
+        self.__campfire_id = campfire_id
+        self.__access_token = session.access_token
+        self.__base_url = session.base_url
+        self.__headers = {
+            'Authorization': 'Bearer '+ self.__access_token,
             "Content-Type": "application/json"
         }        
 
     def info(self):
-        get_campfire_url = f"{self.base_url}/buckets/{self.project_id}/chats/{self.campfire_id}.json"
-        return requests.get(get_campfire_url, headers=self.headers).json()
+        get_campfire_url = f"{self.__base_url}/buckets/{self.__project_id}/chats/{self.__campfire_id}.json"
+        return requests.get(get_campfire_url, headers=self.__headers).json()
     
     def get_lines(self):
-        get_lines_url = f"{self.base_url}/buckets/{self.project_id}/chats/{self.campfire_id}/lines.json"
-        return requests.get(get_lines_url, headers=self.headers).json()
+        get_lines_url = f"{self.__base_url}/buckets/{self.__project_id}/chats/{self.__campfire_id}/lines.json"
+        return requests.get(get_lines_url, headers=self.__headers).json()
     
     def write(self, content):
-        write_url = f"{self.base_url}/buckets/{self.project_id}/chats/{self.campfire_id}/lines.json"
-        requests.post(write_url, headers=self.headers, data='{"content": "'+content+'"}')
+        write_url = f"{self.__base_url}/buckets/{self.__project_id}/chats/{self.__campfire_id}/lines.json"
+        requests.post(write_url, headers=self.__headers, data='{"content": "'+content+'"}')
         print("Sent to campfire successfully!")
