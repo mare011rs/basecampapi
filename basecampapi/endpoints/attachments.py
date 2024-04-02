224d8e42dc20790b0ebfc66d65e18c2fe835c0d6
Marko Stefanovic <mare011rs@gmail.com>
2024-04-02 12:15:58 +0200

    add upload_from_bytes method (#6)
    added a new method to Attachments object
    to allow uploading files from bytes.
    
    Also added filetype package dependency.

--- basecampapi/endpoints/attachments.py
+++ basecampapi/endpoints/attachments.py
@@ -1,6 +1,7 @@
 import os
 from mimetypes import MimeTypes
 
+import filetype
 import requests
 
 from ..basecamp import Basecamp
@@ -13,7 +14,7 @@ class Attachments(Basecamp):
     
     def upload_file(self, path: str, filename):
         '''
-        Uploads a file to Basecamp's servers and saves the file sgid in Basecamp().files.
+        Uploads a file to Basecamp's servers and saves the file sgid in Attachment().files.
 
         Parameters:
             path (str): Path to file you wish to upload.
@@ -38,6 +39,39 @@ class Attachments(Basecamp):
         
         self.files[filename] = {
             "filename": filename,
+            "file_size": str(file_size),
+            "content-type": mime,
+            "sgid": sgid
+        }
+
+    
+    def upload_from_bytes(self, variable, title):
+        '''
+        Uploads a file from bytes to Basecamp's servers and saves the file sgid in Attachment().files.
+
+        Parameters:
+            variable (str): Variable containing the bytes.
+            title: Name of your upload.
+        '''
+        
+        attachments_url = f"{self.__base_url}/attachments.json?name={title}"
+        file_size = len(variable)
+        mime = filetype.guess(variable).mime
+        headers = {
+            'Authorization': 'Bearer '+ self.__credentials['access_token'],
+            "Content-Type": mime,
+            "Content-Length": str(file_size)
+            }
+
+        response = requests.post(attachments_url, headers=headers, data=variable)
+        
+        if not response.ok:
+            raise Exception(f"Status code: {response.status_code}. {response.reason}. Error text: {response.text}.")
+        else:
+            sgid = response.json()['attachable_sgid']
+        
+        self.files[title] = {
+            "filename": title,
             "file_size": str(file_size),
             "content-type": mime,
             "sgid": sgid
